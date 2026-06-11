f1128c042fbfd1d8ace19110d1c401c98a7a84d9
uchiwanagato22 <uchiwanagato22@email.com>
2026-06-11 02:31:31 +0000

    fix desugar version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     // https://firebase.google.com/docs/android/setup#available-libraries
     
     // Core library desugaring for Java 8 API support
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring 'com.android.tools:desugar_jdk_libs:2.1.4'
 }
 
 flutter {
