2aa46d66160667e614653c46feabc70feab462d7
uchiwanagato22 <uchiwanagato22@email.com>
2026-06-14 00:22:00 +0000

    Fix: Configuration Firebase Android et build final Shokugeki

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,16 +1,12 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
-import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
 
+/// DefaultFirebaseOptions configuré spécifiquement pour Shokugeki Menu sur Android.
+/// Ce fichier utilise tes vrais identifiants Firebase pour débloquer le démarrage sur ton téléphone.
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
-    if (kIsWeb) {
-      throw UnsupportedError(
-        'Le support Web nécessite une configuration manuelle. Utilisez Android pour tester avec google-services.json.',
-      );
-    }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
-        // Sous Android, Firebase va lire directement ton fichier google-services.json automatiquement !
         return android;
       default:
         throw UnsupportedError(
@@ -19,11 +15,12 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // Configuration automatique via google-services.json sous Android
+  // Configuration Android officielle tirée de ton google-services.json
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: "FROM_GOOGLE_SERVICES_JSON",
-    appId: "FROM_GOOGLE_SERVICES_JSON",
-    messagingSenderId: "FROM_GOOGLE_SERVICES_JSON",
-    projectId: "FROM_GOOGLE_SERVICES_JSON", // Il va détecter ton projet tout seul
+    apiKey: "AIzaSyAWkRob5TLr2qRs1cPxZcY8R83c_i6RaXI",
+    appId: "1:591627114214:android:d0b8b5887d9328cc078d27",
+    messagingSenderId: "591627114214", // Ton project_number
+    projectId: "shokugeki-menu",
+    storageBucket: "shokugeki-menu.firebasestorage.app",
   );
 }
